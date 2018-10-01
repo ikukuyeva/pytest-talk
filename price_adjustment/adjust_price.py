730b05f7bd513209e24cda6620876173d8130a89
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 20:13:13 -0700

    adjuadjust_price.py: Add another (simple) function to compute price after taxes are added, to help et more intuition about testing framework

--- price_adjustment/adjust_price.py
+++ price_adjustment/adjust_price.py
@@ -5,3 +5,8 @@
 def price_after_discount(price, discount_percent):
 	"""Function to compute price after discount is applied."""
 	return price*(1-discount_percent)
+
+
+def price_after_tax(price, tax_percent):
+	"""Function to compute price after taxes are included."""
+	return price*(1+tax_percent)

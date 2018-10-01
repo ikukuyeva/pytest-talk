7c08737d9512eb95cc35745b5c2ed279033fc6ec
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 20:14:00 -0700

    test_adjust_price.py: Add unit test to show how to use price_after_discount() fcn

---
+++ price_adjustment/tests/test_adust_price.py
@@ -0,0 +1,6 @@
+from price_adjustment.adjust_price import price_after_discount, price_after_tax
+
+def test_price_adjustment_discount():
+	expected_output_price = 75.0
+	output_price = price_after_discount(100.0, 0.25)
+	assert output_price == expected_output_price

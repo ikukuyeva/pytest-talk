f1c28f4b66cd3eb33354041777f65e105d229453
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 21:38:51 -0700

    Add spacing around computation to be more readable

--- price_adjustment/adjust_price.py
+++ price_adjustment/adjust_price.py
@@ -4,9 +4,10 @@
 
 def price_after_discount(price, discount_percent):
 	"""Function to compute price after discount is applied."""
-	return price*(1-discount_percent)
+	return price * (1 - discount_percent)
 
 
 def price_after_tax(price, tax_percent):
 	"""Function to compute price after taxes are included."""
-	return price*(1+tax_percent)
+	return price * (1 + tax_percent)
+

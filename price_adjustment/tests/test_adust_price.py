621a7c3824170bedca7d9faefde76be3b267f1b4
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 22:07:50 -0700

    test_adjust_price.py: Add unit test for price_after_tax() fcn to improve code coverage

--- price_adjustment/tests/test_adust_price.py
+++ price_adjustment/tests/test_adust_price.py
@@ -14,6 +14,17 @@ def test_price_adjustment_discount():
     assert math.fabs(output_price - expected_output_price) < ROUND_OFF_ERROR, \
         """Should show that the price is now reduced by 25%."""
 
+
+def test_price_adjustment_tax():
+    """Unit test to showcase functionality of applying a tax rate to an item's
+       price.
+    """
+    expected_output_price = 110.0
+    output_price = price_after_tax(100.0, 0.10)
+    assert math.fabs(output_price - expected_output_price) < ROUND_OFF_ERROR, \
+        """Should show that the price is now increased by 10%."""
+
+
 def test_price_adjustment_discount_throws_error_price_below_zero():
     """Unit test to showcase edge case behavior of throwing an error when
        the final price of the item after discount is less than 0.

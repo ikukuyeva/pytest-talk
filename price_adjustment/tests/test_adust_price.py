862a9d7dfa2111823f443cecbfec81cc521f5a72
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 21:44:23 -0700

    test_adjust_price.py: Add regression test to show that error is thrown when price is below 0

--- price_adjustment/tests/test_adust_price.py
+++ price_adjustment/tests/test_adust_price.py
@@ -5,3 +5,7 @@ def test_price_adjustment_discount():
 	output_price = price_after_discount(100.0, 0.25)
 	assert output_price == expected_output_price, \
 		"""Should show that the price is now reduced by 25%."""
+
+def test_price_adjustment_discount_throws_error_price_below_zero():
+	with pytest.raises(ValueError):
+		price_after_discount(100.0, 1.25)

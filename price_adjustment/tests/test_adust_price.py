12fac108105dc3d76071f2addb687a2016fea465
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 21:43:43 -0700

    test_adjust_price.py: Add message to assert statement to help explain what should be expected if test breaks

--- price_adjustment/tests/test_adust_price.py
+++ price_adjustment/tests/test_adust_price.py
@@ -3,4 +3,5 @@ from price_adjustment.adjust_price import price_after_discount, price_after_tax
 def test_price_adjustment_discount():
 	expected_output_price = 75.0
 	output_price = price_after_discount(100.0, 0.25)
-	assert output_price == expected_output_price
+	assert output_price == expected_output_price, \
+		"""Should show that the price is now reduced by 25%."""

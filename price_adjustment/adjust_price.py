0d2e7474cff11f7db396cd5360c88e4ddeb52b73
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-09-30 21:41:46 -0700

    adjust_price.py: Add more concise version of fcn for making price adjustment for use with parametrized test example

--- price_adjustment/adjust_price.py
+++ price_adjustment/adjust_price.py
@@ -1,6 +1,7 @@
 """Module containing helper function(s) to find price after discount and/or
    taxes.
 """
+import math
 
 def price_after_discount(price, discount_percent):
 	"""Function to compute price after discount is applied."""
@@ -11,3 +12,9 @@ def price_after_tax(price, tax_percent):
 	"""Function to compute price after taxes are included."""
 	return price * (1 + tax_percent)
 
+
+def price_adjustment(price, adjustment):
+	if math.fabs(adjustment) > 1:
+		raise ValueError("Error: Adjustment is more than 100% of price.")
+	else:
+		return price * (1 + adjustment)

0ece4f4f789fc405f3a9402864cc0843a839d2f1
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-10-07 16:36:33 -0700

    test_adjust_price.py: Add parametrized unit test for price_adjustment() fcn to show how can write unit tests and DRY

--- price_adjustment/tests/test_adust_price.py
+++ price_adjustment/tests/test_adust_price.py
@@ -1,9 +1,12 @@
 """Module for unit, regression and paramterized testing of 'adjust_price'
    functions.
 """
-from price_adjustment.adjust_price import price_after_discount, price_after_tax
 import math
 ROUND_OFF_ERROR = 0.001
+import pytest
+from price_adjustment.adjust_price import price_after_discount, price_after_tax,\
+                                          price_adjustment
+
 
 def test_price_adjustment_discount():
     """Unit test to showcase functionality of applying a discount to an item's
@@ -31,3 +34,23 @@ def test_price_adjustment_discount_throws_error_price_below_zero():
     """
     with pytest.raises(ValueError):
         price_after_discount(100.0, 1.25)
+
+
+@pytest.mark.parametrize("price, adjustment, expected_price", [
+    pytest.param(100, -1.25, 0,
+                 marks=pytest.mark.xfail),
+    (100, -0.1, 90),
+    (100, 0.1, 110),
+    (100, 0.4, 140),
+    pytest.param(100, 1.35, 0,
+                 marks=pytest.mark.xfail)
+    ])
+def test_price_adjustment_positive_and_negative(price, adjustment, expected_price):
+    """Parametrized unit test to showcase how one unit test can be written to
+       be able to test multiple scenarios, and get better code coverage.
+    """
+    output_price = price_adjustment(price, adjustment)
+    assert math.fabs(output_price - expected_price) < ROUND_OFF_ERROR,\
+        """Should show that the price is increased/decreased depending
+           on sign of adjustment.
+        """

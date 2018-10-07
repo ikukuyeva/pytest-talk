8d405a72fd5b7f8879d9ad3de3610908e59fd642
Irina Kukuyeva <ikukuyeva@gmail.com>
2018-10-07 16:39:27 -0700

    Address linter feedback

--- price_adjustment/__init__.py
+++ price_adjustment/__init__.py
@@ -1,1 +1,6 @@
+"""Module containing global variables for use in package.
+"""
+
+# Global variable used in testing suite, to compare that two floats are
+# equivalent, up to round-off error:
 ROUND_OFF_ERROR = 0.001
